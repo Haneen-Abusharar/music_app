0b4078814776c7476db34ce2b814f122eb768eec
Haneen <haneensharar00@gmail.com>
2023-07-27 11:56:28 +0300

    init repository

---
+++ src/pages/library.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from 'next/head'
+const library = () => {
+ return (
+   <div>
+      <Head>
+       <title>Library</title>
+       <meta name="keywords" content="music, streaming, entertainment"></meta>
+     </Head>
+     <h1>Music LIbrary here:</h1>
+   </div>
+);
+};
+export default library;
