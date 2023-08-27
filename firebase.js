0cfa7120f722f4ef6bea2a8ac037cbf9df54d595
Haneen <haneensharar00@gmail.com>
2023-08-27 14:26:45 +0300

    faker config

--- firebase.js
+++ firebase.js
@@ -1,17 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import firebase from "firebase";
+import "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDc2Efs9p_I1pDH-tQ74cqbymq9CnyKTQc",
-  authDomain: "music-application-599cb.firebaseapp.com",
-  projectId: "music-application-599cb",
-  storageBucket: "music-application-599cb.appspot.com",
-  messagingSenderId: "556574564138",
-  appId: "1:556574564138:web:f67deaec3cee9b0be64853"
+  apiKey: "AIzaSyAkSV9X2Id0Ih0DpGD8IXfe4maaQhS_GsA",
+  authDomain: "music-application-bdc7b.firebaseapp.com",
+  databaseURL: "https://music-application-bdc7b-default-rtdb.firebaseio.com",
+  projectId: "music-application-bdc7b",
+  storageBucket: "music-application-bdc7b.appspot.com",
+  messagingSenderId: "821102826577",
+  appId: "1:821102826577:web:9c1ca3dea3e0c635649470",
+  measurementId: "G-Y3853M3W9H"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+!firebase.apps.length
+ ? firebase.initializeApp(firebaseConfig).firestore()
+: firebase.app().firestore();
+var db = firebase.firestore();
+export default db
--- functions/seed.js
+++ functions/seed.js
@@ -1,8 +1,14 @@
 const admin = require("firebase-admin");
-var serviceAccount = require("AIzaSyAfJZJUTryZ2BNyMah4ee3W9ZIx0D4_2oM");
+var serviceAccount = require("./service_account");
 
-const faker = require("faker");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://music-application-bdc7b-default-rtdb.firebaseio.com'
+  });
+const {faker} = require("@faker-js/faker");
+
 const db = admin.firestore();
+
 const fakeIt = () => {
  return db.collection("Music").add({
    username: faker.internet.userName(),
@@ -10,4 +16,5 @@ const fakeIt = () => {
    avatar: faker.internet.avatar(),
 });
 };
+
 Array(5).fill(0).forEach(fakeIt);
